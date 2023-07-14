3aef778ea057ddb36e263d73642b3585084a482c
Govarthan M N <govarthan@ariadnemaps.com>
2023-07-14 14:58:02 +0200

    installed packages

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/govarthan/Documents/GitHub/Ariadne-next13/app/[lang]/layout\":[\"static/media/2aaf0723e720e8b9-s.p.woff2\",\"static/media/86fdec36ddd9097e-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
