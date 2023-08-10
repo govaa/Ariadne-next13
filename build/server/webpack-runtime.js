4f366161794e3dd29c10d83913d3288eefe6bbee
Govarthan M N <govarthan@ariadnemaps.com>
2023-08-10 12:28:35 +0200

    logo carousel bug fix

--- build/server/webpack-runtime.js
+++ build/server/webpack-runtime.js
@@ -119,13 +119,13 @@
 /******/ 		// This function allow to reference async chunks and sibling chunks for the entrypoint
 /******/ 		__webpack_require__.u = (chunkId) => {
 /******/ 			// return url for filenames based on template
-/******/ 			return undefined;
+/******/ 			return "" + chunkId + ".js";
 /******/ 		};
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("11d7b067766e2239")
+/******/ 		__webpack_require__.h = () => ("56b81b14f0ea83fe")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- build/static/chunks/webpack.js
+++ build/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d94d49159edbcb11"; }
+/******/ 		__webpack_require__.h = function() { return "91e01a30a0dc3dce"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
